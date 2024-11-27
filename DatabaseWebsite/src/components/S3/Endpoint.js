4656030a1e78d0bbec5d92e77bcb22b463ab73e0
Gabriel Azar <gabriel@azarcircle.com>
2024-11-27 04:28:26 -0800

    S3 Change

--- DatabaseWebsite/src/components/S3/Endpoint.js
+++ DatabaseWebsite/src/components/S3/Endpoint.js
@@ -1,26 +1,84 @@
-   const AWS = require('aws-sdk');
-   const s3 = new AWS.S3({
-       region: 'US East (N. Virginia) us-east-1',
-       
-   });
+import { S3Client } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
 
-   app.post('/get-signed-url', (req, res) => {
-       const fileName = req.body.fileName;
-       const fileType = req.body.fileType;
+import {
+  ProgressBar,
+  logger,
+} from "@aws-doc-sdk-examples/lib/utils/util-log.js";
 
-       const params = {
-           Bucket: 'my-aws-project-bucket-0987',
-           Key: fileName,
-           Expires: 600, 
-           ContentType: fileType,
-       };
+const twentyFiveMB = 25 * 1024 * 1024;
 
-       s3.getSignedUrl('putObject', params, (err, url) => {
-           if (err) {
-               console.log(err);
-               res.status(500).send(err);
-           } else {
-               res.json({ url });
-           }
-       });
-   });
+export const createString = (size = twentyFiveMB) => {
+  return "x".repeat(size);
+};
+
+/**
+ * Create a 25MB file and upload it in parts to the specified
+ * Amazon S3 bucket.
+ * @param {{ bucketName: string, key: string }}
+ */
+export const main = async ({ bucketName, key }) => {
+  const str = createString();
+  const buffer = Buffer.from(str, "utf8");
+  const progressBar = new ProgressBar({
+    description: `Uploading "${key}" to "${bucketName}"`,
+    barLength: 30,
+  });
+
+  try {
+    const upload = new Upload({
+      client: new S3Client({}),
+      params: {
+        Bucket: bucketName,
+        Key: key,
+        Body: buffer,
+      },
+    });
+
+    upload.on("httpUploadProgress", ({ loaded, total }) => {
+      progressBar.update({ current: loaded, total });
+    });
+
+    await upload.done();
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "AbortError") {
+      logger.error(`Multipart upload was aborted. ${caught.message}`);
+    } else {
+      throw caught;
+    }
+  }
+};
+
+// snippet-end:[javascript.v3.s3.scenarios.multipartupload]
+
+// Call function if run directly
+import { parseArgs } from "node:util";
+import {
+  isMain,
+  validateArgs,
+} from "@aws-doc-sdk-examples/lib/utils/util-node.js";
+
+const loadArgs = () => {
+  const options = {
+    bucketName: {
+      type: "string",
+      required: true,
+    },
+    key: {
+      type: "string",
+      required: true,
+    },
+  };
+  const results = parseArgs({ options });
+  const { errors } = validateArgs({ options }, results);
+  return { errors, results };
+};
+
+if (isMain(import.meta.url)) {
+  const { errors, results } = loadArgs();
+  if (!errors) {
+    main(results.values);
+  } else {
+    logger.error(errors.join("\n"));
+  }
+}
