f4675955cb854e7c8f15c400733d22fc059d3466
Christian Anderson <canderson@cpp.edu>
2024-12-02 11:17:22 -0800

    implement svg icons for buttons on the home feed to look pretty.

---
+++ DatabaseWebsite/src/pages/svgIcons.js
@@ -0,0 +1,508 @@
+export const searchIcon = (
+  <svg
+    aria-label="Search"
+    color="#8e8e8e"
+    fill="#8e8e8e"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <path
+      d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+    <line
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      x1="16.511"
+      x2="22"
+      y1="16.511"
+      y2="22"
+    ></line>
+  </svg>
+);
+
+export const savedHome = (
+  <svg
+    aria-label="Remove"
+    className="_ab6-"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M20 22a.999.999 0 0 1-.687-.273L12 14.815l-7.313 6.912A1 1 0 0 1 3 21V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1Z"></path>
+  </svg>
+);
+
+export const commentMore = (
+  <svg aria-label="Comment options" className="_ab6-" color="#8e8e8e" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
+)
+
+export const saveHome = (
+  <svg
+    aria-label="Remove"
+    className="_ab6-"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M20 22a.999.999 0 0 1-.687-.273L12 14.815l-7.313 6.912A1 1 0 0 1 3 21V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1Z"></path>
+  </svg>
+);
+
+export const afterLike = (
+  <svg
+    aria-label="Unlike"
+    color="#FF4500"
+    fill="#FF4500"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M12 2l8 8h-6v12h-4V10H4l8-8z"></path>
+  </svg>
+);
+
+export const homeOutline = (
+  <svg
+    aria-label="Home"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path
+      d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z"
+      fill="none"
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+  </svg>
+);
+export const homeFill = (
+  <svg
+    aria-label="Home"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"></path>
+  </svg>
+);
+
+export const messageOutline = (
+  <svg
+    aria-label="Messenger"
+    color="#262626"
+    fill="none"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+    stroke="#262626"
+    strokeWidth="2"
+  >
+    <path
+      d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H8l-4 4V6c0-1.1.9-2 2-2z"
+      fill="none"
+      stroke="currentColor"
+    ></path>
+    <circle cx="8" cy="12" r="1.5" fill="currentColor"></circle>
+    <circle cx="12" cy="12" r="1.5" fill="currentColor"></circle>
+    <circle cx="16" cy="12" r="1.5" fill="currentColor"></circle>
+  </svg>
+);
+
+export const messageFill = (
+  <svg
+    aria-label="Messenger"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H8l-4 4V6c0-1.1.9-2 2-2z"></path>
+    <circle cx="8" cy="12" r="1.5" fill="white"></circle>
+    <circle cx="12" cy="12" r="1.5" fill="white"></circle>
+    <circle cx="16" cy="12" r="1.5" fill="white"></circle>
+  </svg>
+);
+
+
+export const exploreOutline = (
+  <svg
+    aria-label="Find People"
+    color="#262626"
+    fill="none"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+    stroke="#262626"
+    strokeWidth="2"
+  >
+    <circle cx="10" cy="10" r="8" fill="none" stroke="currentColor"></circle>
+    <line x1="10" y1="2" x2="10" y2="18" stroke="currentColor"></line>
+    <line x1="2" y1="10" x2="18" y2="10" stroke="currentColor"></line>
+    <circle cx="17" cy="17" r="3" fill="none" stroke="currentColor"></circle>
+    <line x1="19.5" y1="19.5" x2="22" y2="22" stroke="currentColor"></line>
+  </svg>
+);
+
+export const likeFill = (
+  <svg
+    aria-label="Notifications"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M12 2a7 7 0 0 0-7 7v4.5a3 3 0 0 1-.82 2.07L3 17.5h18l-1.18-1.93A3 3 0 0 1 19 13.5V9a7 7 0 0 0-7-7zm-1 19a2 2 0 0 0 4 0h-4z"></path>
+  </svg>
+);
+
+export const likeOutline = (
+  <svg
+    aria-label="Activity Feed"
+    color="#000000"
+    fill="#000000"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M12 2l8 8h-6v12h-4V10H4l8-8z"></path>
+  </svg>
+);
+
+export const bellOutline = (
+  <svg
+    aria-label="Notifications"
+    color="#262626"
+    fill="none"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+    stroke="#262626"
+    strokeWidth="2"
+  >
+    <path
+      d="M12 2a7 7 0 0 0-7 7v4.5a3 3 0 0 1-.82 2.07L3 17.5h18l-1.18-1.93A3 3 0 0 1 19 13.5V9a7 7 0 0 0-7-7z"
+      fill="none"
+    ></path>
+    <path d="M11 21a2 2 0 0 0 4 0h-4z" fill="none"></path>
+  </svg>
+);
+
+
+export const postUploadOutline = (
+  <svg
+    aria-label="New Post"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path
+      d="M12 2l6 6h-4v10h-4V8H6l6-6zM4 20h16v2H4z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const profileIcon = (
+  <svg
+    aria-label="Profile"
+    color="#262626"
+    fill="#262626"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <circle
+      cx="12.004"
+      cy="12.004"
+      fill="none"
+      r="10.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="2"
+    ></circle>
+    <path
+      d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="2"
+    ></path>
+    <circle
+      cx="12.006"
+      cy="9.718"
+      fill="none"
+      r="4.109"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="2"
+    ></circle>
+  </svg>
+);
+export const savedIcon = (
+  <svg
+    aria-label="Saved"
+    color="#262626"
+    fill="#262626"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <polygon
+      fill="none"
+      points="20 21 12 13.44 4 21 4 3 20 3 20 21"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></polygon>
+  </svg>
+);
+
+export const exploreFill = (
+  <svg
+    aria-label="Find people"
+    className="_ab6-"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <circle cx="10" cy="10" r="8" fill="currentColor"></circle>
+    <circle cx="17" cy="17" r="3" fill="currentColor"></circle>
+    <line x1="19.5" y1="19.5" x2="22" y2="22" stroke="currentColor" strokeWidth="2"></line>
+  </svg>
+);
+
+export const settingsIcon = (
+  <svg
+    aria-label="Settings"
+    color="#262626"
+    fill="#262626"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      fill="none"
+      r="8.635"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></circle>
+    <path
+      d="M14.232 3.656a1.269 1.269 0 01-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 01-.796.66m-.001 16.688a1.269 1.269 0 01.796.66l.505.996h1.862l.505-.996a1.269 1.269 0 01.796-.66M3.656 9.768a1.269 1.269 0 01-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 01.66.796m16.688-.001a1.269 1.269 0 01.66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 01-.66-.796M7.678 4.522a1.269 1.269 0 01-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 01-.096 1.03m11.8 11.799a1.269 1.269 0 011.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 01.096-1.03m-14.956.001a1.269 1.269 0 01.096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 011.03.096m11.799-11.8a1.269 1.269 0 01-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 01-1.03-.096"
+      fill="none"
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+  </svg>
+);
+export const switchAccountIcon = (
+  <svg
+    aria-label="Switch Accounts"
+    color="#262626"
+    fill="#262626"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <path d="M8 8.363a1 1 0 00-1-1H4.31a8.977 8.977 0 0114.054-1.727 1 1 0 101.414-1.414A11.003 11.003 0 003 5.672V3.363a1 1 0 10-2 0v5a1 1 0 001 1h5a1 1 0 001-1zm14 6.274h-5a1 1 0 000 2h2.69a8.977 8.977 0 01-14.054 1.727 1 1 0 00-1.414 1.414A11.004 11.004 0 0021 18.33v2.307a1 1 0 002 0v-5a1 1 0 00-1-1z"></path>
+  </svg>
+);
+
+export const moreIcons = (
+  <svg
+    aria-label="More options"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <circle cx="12" cy="12" r="1.5"></circle>
+    <circle cx="6" cy="12" r="1.5"></circle>
+    <circle cx="18" cy="12" r="1.5"></circle>
+  </svg>
+);
+
+export const likeIconOutline = (
+  <svg
+    aria-label="Like"
+    color="#FF4500"
+    fill="#FF4500"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M12 2l8 8h-6v12h-4V10H4l8-8z"></path>
+  </svg>
+);
+
+export const likeIconFill = (
+  <svg
+    aria-label="Unlike"
+    color="#FF4500"
+    fill="#FF4500"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M12 2l8 8h-6v12h-4V10H4l8-8z"></path>
+  </svg>
+);
+
+export const commentIcon = (
+  <svg
+    aria-label="Comment"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path
+      d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z"
+      fill="none"
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+  </svg>
+);
+
+export const shareIcon = (
+  <svg
+    aria-label="Share Post"
+    color="#000000"
+    fill="none"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+    stroke="#000000"
+    strokeWidth="2"
+  >
+    <path
+      d="M12 2l6 6h-4v6h-4V8H6l6-6z"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M4 10v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
+
+
+export const saveIconOutline = (
+  <svg
+    aria-label="Save"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <polygon
+      fill="none"
+      points="20 21 12 13.44 4 21 4 3 20 3 20 21"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></polygon>
+  </svg>
+);
+
+export const saveIconFill = (
+  <svg
+    aria-label="Remove"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M20 22a.999.999 0 01-.687-.273L12 14.815l-7.313 6.912A1 1 0 013 21V3a1 1 0 011-1h16a1 1 0 011 1v18a1 1 0 01-1 1z"></path>
+  </svg>
+);
+
+export const emojiIcon = (
+  <svg
+    aria-label="Emoji"
+    color="#262626"
+    fill="#262626"
+    height="24"
+    role="img"
+    viewBox="0 0 24 24"
+    width="24"
+  >
+    <path d="M15.83 10.997a1.167 1.167 0 101.167 1.167 1.167 1.167 0 00-1.167-1.167zm-6.5 1.167a1.167 1.167 0 10-1.166 1.167 1.167 1.167 0 001.166-1.167zm5.163 3.24a3.406 3.406 0 01-4.982.007 1 1 0 10-1.557 1.256 5.397 5.397 0 008.09 0 1 1 0 00-1.55-1.263zM12 .503a11.5 11.5 0 1011.5 11.5A11.513 11.513 0 0012 .503zm0 21a9.5 9.5 0 119.5-9.5 9.51 9.51 0 01-9.5 9.5z"></path>
+  </svg>
+);
+
+
+export const galleryIcon = (
+  <svg aria-label="Add Photo or Video" className="_ab6-" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M6.549 5.013A1.557 1.557 0 1 0 8.106 6.57a1.557 1.557 0 0 0-1.557-1.557Z" fillRule="evenodd"></path><path d="m2 18.605 3.901-3.9a.908.908 0 0 1 1.284 0l2.807 2.806a.908.908 0 0 0 1.283 0l5.534-5.534a.908.908 0 0 1 1.283 0l3.905 3.905" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path><path d="M18.44 2.004A3.56 3.56 0 0 1 22 5.564h0v12.873a3.56 3.56 0 0 1-3.56 3.56H5.568a3.56 3.56 0 0 1-3.56-3.56V5.563a3.56 3.56 0 0 1 3.56-3.56Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path></svg>
+)
