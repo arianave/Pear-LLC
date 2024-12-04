1c804bd44d156b06966122eb90a142f0af4d4249
Christian Anderson <canderson@cpp.edu>
2024-12-03 21:03:30 -0800

    fixed a spelling error

--- DatabaseWebsite/src/Server/server.js
+++ DatabaseWebsite/src/Server/server.js
@@ -5,7 +5,7 @@ const app = express();
 process.env.PORT = 3000;
 const port = process.env.PORT || 5000;
 import multer from 'multer';
-import { S3Client, PutObjectAclCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from 'dotenv';
 import crypto from 'crypto';
