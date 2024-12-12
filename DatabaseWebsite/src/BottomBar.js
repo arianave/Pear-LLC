b524ba5cbd9290358b07de143f32f0d8429263e6
arianave <arianavillagrana@yahoo.com>
2024-12-12 15:42:32 -0800

    fixing UI

--- DatabaseWebsite/src/BottomBar.js
+++ DatabaseWebsite/src/BottomBar.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import './BottomBar.css'; // CSS for the bottom bar
 import { getUserId } from './userData/user';
 
+//import icons
+import feedIcon from "./Icons/feedIcon.png";
+import messageIcon from "./Icons/messageIcon.png";
+import profileIcon from "./Icons/profileIcon.png";
+import postIcon from "./Icons/postIcon.png";
+
 function BottomBar() {
   const userId = getUserId();
 
@@ -10,10 +16,18 @@ function BottomBar() {
     <div className="bottom-bar">
         {/* Need to include feed button and message button once implemented */}
          {/* Using link here instead of a will allower more usability as it disables full page reload*/}
-      <Link to="/HomeFeed">Feed</Link>
-      <Link to="/PostCreation">Post</Link>
-      <Link to="/Messages">Message</Link>  
-      <Link to={`/ProfilePage/${userId}`}>Profile</Link>
+      <Link to="/HomeFeed">
+        <img src={feedIcon} alt="Feed" className="bottom-bar-icon" />
+      </Link>
+      <Link to="/PostCreation">
+        <img src={postIcon} alt="post" className="post-icon" />
+      </Link>
+      <Link to="/Messages">
+        <img src={messageIcon} alt="message" className="message-icon" />
+      </Link>
+      <Link to={`/ProfilePage/${userId}`}>
+        <img src={profileIcon} alt="profile" className="profile-icon" />
+      </Link>
     </div>
   );
 }
